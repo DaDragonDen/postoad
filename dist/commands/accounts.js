91dc803a94885a530112f45c73b1c0e2e5900056
Christian Toney <christian@makuwro.com>
2024-12-17 13:21:50 -0500

    Delete dist directory

--- dist/commands/accounts.js
+++
@@ -1,9 +0,0 @@
-import Command from "#utils/Command.js";
-import addAccountCommand from "./accounts/authorize.js";
-const command = new Command({
-    name: "accounts",
-    description: "Add a Bluesky account with an app password.",
-    subCommands: [addAccountCommand]
-});
-export default command;
-//# sourceMappingURL=accounts.js.map
