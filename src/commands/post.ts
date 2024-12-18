11028b05003a311b6cee0508f78a4c770ff27d92
Christian Toney <christian@makuwro.com>
2024-12-18 15:01:45 -0500

    Add feature: Automatically post text and media from a Discord channel (#20)
    * Update
    
    * Update attachment source
    
    * Allow image uploads
    
    * Add videos

--- src/commands/post.ts
+++ src/commands/post.ts
@@ -32,7 +32,7 @@ const command = new Command({
 
       if (!handlePairs[0]) {
 
-        throw new Error("You must authorize Postoad to use a Bluesky account before you use this command.")
+        throw new Error("You must authorize Postoad to use a Bluesky account before you use this command.");
 
       }
 
@@ -66,12 +66,21 @@ const command = new Command({
       const originalEmbed = originalResponse?.embeds?.[0];
       if (!originalEmbed) throw new Error("Something bad happened. Try again later.");
 
+      const description = (shouldUseEmbedDescription ? originalEmbed.description : newPostContent) || undefined;
+      const attachmentSources = originalEmbed.fields?.[0]?.value ?? "-# Reply to this message with any media that you want to add.";
+
       await interaction.editOriginal({
-        content: "Check this out — make sure it looks good. When you're ready, hit submit!",
+        content: "Check this out — make sure it looks good. When you're ready, hit submit!\n-# Note: Bluesky limits the images and [videos](https://bsky.social/about/blog/09-11-2024-video) that you can post.",
         embeds: [
           {
             ...originalEmbed,
-            description: (shouldUseEmbedDescription ? originalEmbed.description : newPostContent) || undefined
+            description,
+            fields: [
+              {
+                name: "Attachment source",
+                value: attachmentSources
+              }
+            ]
           }
         ],
         components: [
@@ -82,7 +91,8 @@ const command = new Command({
                 type: ComponentTypes.BUTTON,
                 customID: "post/submitPost",
                 style: ButtonStyles.PRIMARY,
-                label: "Submit post"
+                label: "Submit post",
+                disabled: attachmentSources.includes("Reply") && !description
               },
               {
                 type: ComponentTypes.BUTTON,
@@ -156,33 +166,38 @@ const command = new Command({
           // Update the message.
           const originalResponse = await interaction.getOriginal();
           const originalEmbed = originalResponse.embeds?.[0];
-          const authorSection = originalEmbed?.author;
-          const did = "values" in interaction.data ? interaction.data.values.getStrings().join() : originalEmbed?.footer?.text;
-          const handle = !authorSection ? (await blueskyClient.didResolver.resolve(did as Did)).alsoKnownAs?.[0].replace("at://", "") : undefined;
-          if (!did) {
+          let handle;
+          let did;
+          if (interaction.data.customID === "post/accountSelector") {
 
-            await interaction.editOriginal({
-              content: "Something bad happened. Try again later.",
-              embeds: [],
-              components: []
-            });
+            did = "values" in interaction.data ? interaction.data.values.getStrings().join() : originalEmbed?.footer?.text;
+            handle = (await blueskyClient.didResolver.resolve(did as Did)).alsoKnownAs?.[0].replace("at://", "");
+            if (!did) {
 
-            return;
+              await interaction.editOriginal({
+                content: "Something bad happened. Try again later.",
+                embeds: [],
+                components: []
+              });
 
+              return;
+
+            }
+            
           }
 
           await interaction.editOriginal({
             content: "What do you want the message to say? Respond in the modal.",
-            embeds: [
+            embeds: handle && did ? [
               {
-                author: handle ? {
+                author: {
                   name: handle
-                } : authorSection,
+                },
                 footer: {
                   text: did
                 }
               }
-            ],
+            ] : originalResponse.embeds,
             components: []
           });
 
@@ -199,22 +214,103 @@ const command = new Command({
           const originalEmbed = originalResponse?.embeds?.[0];
           const did = originalEmbed?.footer?.text;
           const text = originalEmbed?.description;
-          if (!did || !text) {
+          if (!did) {
 
             await interaction.editOriginal({
               content: "Something bad happened. Try again later.",
               embeds: [],
               components: []
-            })
+            });
             
             return;
 
           }
-
-          // Post to Bluesky.
+          
           const session = await blueskyClient.restore(did);
           const agent = new Agent(session);
-          const post = await agent.post({text});
+          
+          // Try to get images and videos from attachment sources.
+          const attachmentSourceJumpLink = originalEmbed.fields?.[0].value;
+          const blobsWithAltText: [Blob, string?][] = [];
+          let mode: "image" | "video" = "image";
+          if (attachmentSourceJumpLink) {
+
+            // 
+            const attachmentSourceJumpLinkSplits = attachmentSourceJumpLink.split("/");
+            const channelID = attachmentSourceJumpLinkSplits[attachmentSourceJumpLinkSplits.length - 2];
+            const messageID = attachmentSourceJumpLinkSplits[attachmentSourceJumpLinkSplits.length - 1];
+            const message = await interaction.client.rest.channels.getMessage(channelID, messageID);
+            for (const attachment of message.attachments.filter(() => true)) {
+
+              const response = await fetch(attachment.url);
+              if (!response.ok) {
+
+                
+
+              }
+
+              const blob = await response.blob();
+              const altText = attachment.description;
+              blobsWithAltText.push([blob, altText]);
+
+              if (attachment.contentType?.includes("video")) {
+
+                mode = "video";
+
+              }
+
+            }
+
+          }
+
+          // Verify that there is at least text or media.
+          if (!blobsWithAltText[0] && !text) {
+
+            const originalComponent = originalResponse.components?.[0];
+
+            if (!originalComponent) return;
+
+            await interaction.editOriginal({
+              components: [
+                {
+                  ...originalComponent,
+                  components: originalComponent.components.map((component, index) => index === 0 ? {
+                    type: ComponentTypes.BUTTON,
+                    customID: "post/submitPost",
+                    style: ButtonStyles.PRIMARY,
+                    label: "Submit post",
+                    disabled: true
+                  } : component)
+                }
+              ]
+            });
+
+            return;
+
+          }
+
+          const media = [];
+          for (const blob of blobsWithAltText) {
+
+            const {data} = await agent.uploadBlob(blob[0]);
+            media.push({
+              alt: blob[1] ?? "",
+              [mode === "video" ? "video" : "image"]: data.blob
+            });
+
+          }
+
+          // Post to Bluesky.
+          const post = await agent.post({
+            text: text ?? "", 
+            embed: mode === "video" ? {
+              $type: "app.bsky.embed.video",
+              ...media[0]
+            } : {
+              $type: "app.bsky.embed.images",
+              images: media
+            }
+          });
 
           // Give the link to the user.
           const uriSplits = post.uri.split("/");
