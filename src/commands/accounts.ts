577e8b23ebea8d21a92a29239139bc520ec3010b
Christian Toney <christian@makuwro.com>
2024-12-21 12:35:38 -0500

    Add command: /accounts default

--- src/commands/accounts.ts
+++ src/commands/accounts.ts
@@ -1,11 +1,12 @@
 import Command from "#utils/Command.js"
 import authorizeSubCommand from "./accounts/authorize.js";
+import defaultAccountSubCommand from "./accounts/default.js";
 import signoutSubCommand from "./accounts/signout.js";
 
 const accountsCommand = new Command({
   name: "accounts",
   description: "Add a Bluesky account with an app password.",
-  subCommands: [authorizeSubCommand, signoutSubCommand]
+  subCommands: [authorizeSubCommand, signoutSubCommand, defaultAccountSubCommand]
 });
 
 export default accountsCommand;
