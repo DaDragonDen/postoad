dc52b307da7ad30a5b6f33471af4cae922b8a95c
Christian Toney <christian@makuwro.com>
2024-12-21 19:16:42 -0500

    Add feature: /accounts mfa - List accounts

--- src/commands/accounts.ts
+++ src/commands/accounts.ts
@@ -1,12 +1,13 @@
 import Command from "#utils/Command.js"
 import authorizeSubCommand from "./accounts/authorize.js";
 import defaultAccountSubCommand from "./accounts/default.js";
+import mfaSubCommand from "./accounts/mfa.js";
 import signoutSubCommand from "./accounts/signout.js";
 
 const accountsCommand = new Command({
   name: "accounts",
   description: "Add a Bluesky account with an app password.",
-  subCommands: [authorizeSubCommand, signoutSubCommand, defaultAccountSubCommand]
+  subCommands: [authorizeSubCommand, signoutSubCommand, defaultAccountSubCommand, mfaSubCommand]
 });
 
 export default accountsCommand;
